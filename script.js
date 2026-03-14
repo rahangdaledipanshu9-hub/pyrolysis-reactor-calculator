0f1d22f4ba64f1c72ea13786a950204c84172117
rahangdaledipanshu9-hub <rahangdaledipanshu9@gmail.com>
2026-03-15 01:16:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-function calculate(){
+function calculateYield(){
 
-let feed = parseFloat(document.getElementById("feedstock").value);
+let feedstock = parseFloat(document.getElementById("feedstock").value);
 let catalyst = parseFloat(document.getElementById("catalyst").value);
-let temp = parseFloat(document.getElementById("temperature").value);
+let temperature = parseFloat(document.getElementById("temperature").value);
 
-let oil_percent = 60 + (0.8*catalyst) + (0.02*(temp-350));
-let gas_percent = 25 - (0.3*catalyst);
+if(isNaN(feedstock) || isNaN(catalyst) || isNaN(temperature)){
+
+alert("Please enter all values");
+
+return;
+
+}
+
+/* Yield model */
+
+let oil_percent = 60 + (0.5 * catalyst) + (0.02 * (temperature - 350));
+
+let gas_percent = 25 - (0.2 * catalyst);
+
 let char_percent = 100 - oil_percent - gas_percent;
 
-let oil = feed * oil_percent/100;
-let gas = feed * gas_percent/100;
-let char = feed * char_percent/100;
+/* Convert to kg */
 
-document.getElementById("result").innerHTML = 
-"Oil Yield = " + oil.toFixed(2) + " kg<br>" +
-"Gas Yield = " + gas.toFixed(2) + " kg<br>" +
-"Biochar Yield = " + char.toFixed(2) + " kg";
+let oil = (feedstock * oil_percent / 100).toFixed(2);
+
+let gas = (feedstock * gas_percent / 100).toFixed(2);
+
+let char = (feedstock * char_percent / 100).toFixed(2);
+
+/* Display */
+
+document.getElementById("oil").innerText = oil + " kg";
+
+document.getElementById("gas").innerText = gas + " kg";
+
+document.getElementById("char").innerText = char + " kg";
 
 }
